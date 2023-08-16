f8718bfe67064b6e91a2872a3b941f05878957a0
SaulMoreyra <armhacam21@gmail.com>
2023-08-15 23:05:34 -0600

    config(initial): add basic initial config prettier lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,47 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native-community', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+        curly: 'off',
+        'import/order': [
+          1,
+          {
+            groups: [
+              'builtin',
+              'external',
+              'internal',
+              'sibling',
+              'parent',
+              'index',
+            ],
+            pathGroups: [
+              {
+                pattern: 'react+(|-native)',
+                group: 'external',
+                position: 'before',
+              },
+              { pattern: '@core', group: 'internal' },
+              { pattern: '@core/**', group: 'internal' },
+              { pattern: '@e', group: 'internal' },
+              { pattern: '@e/**', group: 'internal' },
+            ],
+            pathGroupsExcludedImportTypes: ['react'],
+            alphabetize: {
+              order: 'asc',
+              caseInsensitive: true,
+            },
+            'newlines-between': 'always',
+          },
+        ],
+      },
+    },
+  ],
 };
