4afa473240638cb3d4787784ac5e6831c86b83ea
SaulMoreyra <armhacam21@gmail.com>
2023-09-12 21:21:27 -0600

    feat(component): add container with spacing

---
+++ src/core/interfaces/ISpacing.ts
@@ -0,0 +1,16 @@
+export type ISpacingContainer = {
+  p?: number;
+  pt?: number;
+  pb?: number;
+  pr?: number;
+  pl?: number;
+  pv?: number;
+  ph?: number;
+  m?: number;
+  mt?: number;
+  mb?: number;
+  mr?: number;
+  ml?: number;
+  mv?: number;
+  mh?: number;
+};
