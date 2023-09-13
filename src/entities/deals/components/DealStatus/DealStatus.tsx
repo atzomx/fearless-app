eb42fbd73e4d3f609395124e5f5f88d544b0ba66
SaulMoreyra <armhacam21@gmail.com>
2023-09-12 21:21:45 -0600

    feat(Deals): add components for deals screen

--- src/entities/deals/components/DealStatus/DealStatus.tsx
+++ src/entities/deals/components/DealStatus/DealStatus.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 
-import { CameraIcon } from '@core/icons';
+import { HandFiveIcon } from '@core/icons';
 
 import { VariantStatus } from './DealStatus.model';
 import * as S from './DealStatus.style';
@@ -13,7 +13,7 @@ const DealStatus: FC<DealStatusProps> = ({ variant, children }) => {
   return (
     <S.Container variant={variant}>
       <S.IconContainer variant={variant}>
-        <CameraIcon />
+        <HandFiveIcon height={18} width={18} color="#00546F" />
       </S.IconContainer>
       <S.Status variant={variant}>{children}</S.Status>
     </S.Container>
