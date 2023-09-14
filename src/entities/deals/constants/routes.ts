c7e2343f553dd7a7fb370fba8108dd72581c42c4
SaulMoreyra <armhacam21@gmail.com>
2023-09-13 21:12:23 -0600

    feat(deals): implement float button a new screen for new deal

--- src/entities/deals/constants/routes.ts
+++ src/entities/deals/constants/routes.ts
@@ -1,5 +1,6 @@
 const DEALS_ROUTES = {
   home: '@deals/home',
+  new: '@deals/new',
 };
 
 export default DEALS_ROUTES;
---
+++ src/entities/deals/screens/NewDealScreen/NewDealScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Text } from '@core/ui';
+import Container from '@core/ui/Container';
+
+const NewDealScreen = () => {
+  return (
+    <Container>
+      <Text>Hola</Text>
+    </Container>
+  );
+};
+
+export default NewDealScreen;
