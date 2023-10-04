3d466541e44bfa41e84ff71bab66ce915bab7526
SaulMoreyra <armhacam21@gmail.com>
2023-10-03 23:06:43 -0600

    feat(icons): add Icons for choose file

---
+++ src/core/icons/CloseIcon.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+import Svg, { SvgProps, Path } from 'react-native-svg';
+
+const CloseIcon = ({ width = 24, height = 24, ...props }: SvgProps) => (
+  <Svg
+    fill="none"
+    stroke="currentColor"
+    strokeWidth={1}
+    viewBox="0 0 24 24"
+    {...{ width, height }}
+    {...props}>
+    <Path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M6 18 18 6M6 6l12 12"
+    />
+  </Svg>
+);
+
+export default CloseIcon;
