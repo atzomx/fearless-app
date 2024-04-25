0c3b9e0ffa225f129594919197b24132d70e6083
SaulMoreyra <armhacam21@gmail.com>
2024-04-24 21:18:04 -0600

    feat(i18n): add new translations

--- src/core/i18n/es/index.ts
+++ src/core/i18n/es/index.ts
@@ -2,5 +2,6 @@ import * as auth from './auth';
 import * as common from './common';
 import * as deals from './deals';
 import * as home from './home';
+import * as profile from './profile';
 
-export default { common, auth, deals, home };
+export default { common, auth, deals, home, profile };
